3ba1f4b8626cec0aac5efe49d698d4401eb4a281
morganmepham <morgancmepham@gmail.com>
2022-10-26 15:00:50 +0100

    Money page 2

--- src/Components/Money_comps/MoneyBody/MoneyBody.js
+++ src/Components/Money_comps/MoneyBody/MoneyBody.js
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import MoneyHeader from '../MoneyHeader/MoneyHeader';
 import MoneySidebar from '../MoneySidebar/MoneySidebar';
 import RecentTransactions from '../RecentTransactions/RecentTransactions';
@@ -5,10 +6,21 @@ import './MoneyBody.css'
 
 
 const MoneyBody = () => {
+    const navigate = useNavigate()
+    const handleAddTransaction = () => {
+        navigate('/transaction')
+    }
+    const handleViewTrans = () => {
+        navigate('/money/all_transactions')
+    }
     return (
         <div className="money-body">
             <MoneyHeader />
             <RecentTransactions />
+            <div className="mobile-money-buttons">
+                <input type="button" className="mobile-view-transactions" value='View Transactions' onClick={handleViewTrans}/>
+                <input type="button" className="mobile-add-transaction" value='Add Transaction' onClick={handleAddTransaction}/>
+            </div>
             <MoneySidebar />
         </div>
     );
